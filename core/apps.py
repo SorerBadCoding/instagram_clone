89908f10f74979cddccf408524629c86a522b41e
SorerBadCoding <Sor.er2013@gmail.com>
2026-06-26 20:59:53 +0700

    Fix apps.py

--- core/apps.py
+++ core/apps.py
@@ -1,16 +1,23 @@
-def ready(self):
-    import core.signals  # noqa: F401
+from django.apps import AppConfig
 
-    try:
-        from django.contrib.auth import get_user_model
+class CoreConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "core"
+    verbose_name = "Instagram Clone Core"
 
-        User = get_user_model()
+    def ready(self):
+        import core.signals  # noqa: F401
 
-        if not User.objects.filter(username="potling").exists():
-            User.objects.create_superuser(
-                username="potling",
-                email="your@email.com",
-                password="Potling123!"
-            )
-    except Exception:
-        pass
+        try:
+            from django.contrib.auth import get_user_model
+
+            User = get_user_model()
+
+            if not User.objects.filter(username="potling").exists():
+                User.objects.create_superuser(
+                    username="potling",
+                    email="Sor.er2013@email.com",
+                    password="Potling123!"
+                )
+        except Exception as e:
+            print("ADMIN CREATE ERROR:", e)
