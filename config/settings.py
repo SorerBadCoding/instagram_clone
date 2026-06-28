0f9e41afae056045a003f4fe21c22a4397764cce
SorerBadCoding <Sor.er2013@gmail.com>
2026-06-28 18:00:09 +0700

    Fix STATIC_URL

--- config/settings.py
+++ config/settings.py
@@ -162,7 +162,7 @@ TIME_ZONE = os.environ.get("DJANGO_TIME_ZONE", "UTC")
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STORAGES = {
